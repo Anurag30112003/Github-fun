af476500d399b6da477bd5637c38ff775ff36cb5
Anurag30112003 <anuragsharma011011@gmail.com>
2021-12-24 15:45:07 +0530

    backend-improvement

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from fetch import *
+import requests
 app = Flask(__name__)  
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html', username=username,location=location,repos = repos)
+    if request.method == 'POST':
+        username = request.form['user']
+        api = requests.get(f'https://api.github.com/users/{username}')
+        name = api.json()['login']
+        url = api.json()['html_url']
+        repos = api.json()['public_repos']
+        location = api.json()['location']
+        return  render_template('index.html', username=username,location=location,repos = repos)
+    else:
+        return render_template('index.html')
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True , port=5000)
--- fetch.py
+++ fetch.py
@@ -1,9 +1,10 @@
-import requests
-username = 'Anurag30112003'
-#fetchs specific parameter from api request
-api = requests.get(f'https://api.github.com/users/{username}')
-name = api.json()['login']
-url = api.json()['html_url']
-repos = api.json()['public_repos']
-location = api.json()['location']
-print(username,url,repos)
+# import requests
+# # from app import user
+# username = user
+# #fetchs specific parameter from api request
+# api = requests.get(f'https://api.github.com/users/{username}')
+# name = api.json()['login']
+# url = api.json()['html_url']
+# repos = api.json()['public_repos']
+# location = api.json()['location']
+# # print(username,url,repos)
