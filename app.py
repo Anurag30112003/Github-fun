60f88d36138987623689c1f9d3c67d095b57e11f
Anurag30112003 <anuragsharma011011@gmail.com>
2021-12-24 20:07:51 +0530

    trying-tailwind

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-from fetch import *
-import requests
-app = Flask(__name__)  
+from main import app
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['user']
-        api = requests.get(f'https://api.github.com/users/{username}')
-        name = api.json()['login']
-        url = api.json()['html_url']
-        repos = api.json()['public_repos']
-        location = api.json()['location']
-        return  render_template('index.html', username=username,location=location,repos = repos)
-    else:
-        return render_template('index.html')
-
-    
-
-if __name__ == '__main__':
-    app.run(debug=True , port=5000)
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
---
+++ static/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './templates/*.html'
+  ],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
